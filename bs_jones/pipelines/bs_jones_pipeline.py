c451ebc92d4430243e38728040abb81a9ad2b76c
JachymDolezal <jachym.dolezal@gmail.com>
2024-02-01 23:09:26 +0100

    add new crontrigger

--- bs_jones/pipelines/bs_jones_pipeline.py
+++ bs_jones/pipelines/bs_jones_pipeline.py
@@ -7,6 +7,7 @@ import bspump.trigger
 import bspump.common
 import bspump.abc.source
 import bspump.kafka
+import datetime
 
 from .generators import SybaseEventGenerator
 
@@ -28,12 +29,13 @@ class BSJonesPipeline(bspump.Pipeline):
 		super().__init__(app, pipeline_id)
 
 		try:
-			self.QueryInterval = int(eval(asab.Config.get("sybase", "query_interval")))
+			# in cron format eg. 0 0 0 * * *
+			self.QueryInterval = asab.Config.get("sybase", "query_interval")
 		except Exception as e:
 			L.debug("query_interval in config must be either an expression or an number {}".format(e))
 
 		self.build(
-			LoadSource(app, self).on(bspump.trigger.OpportunisticTrigger(app, self, chilldown_period=self.QueryInterval)),
+			LoadSource(app, self).on(bspump.trigger.CronTrigger(app, self, self.QueryInterval, datetime.now())),
 			SybaseEventGenerator(app, self),
 			bspump.common.StdDictToJsonParser(app, self),
 			bspump.common.StringToBytesParser(app, self),
